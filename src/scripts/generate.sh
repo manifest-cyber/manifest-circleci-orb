7fcc7c0c674eef8239ec0d5ef50dfb80fa36fbe4
manifestori <ori@manifestcyber.com>
2023-11-01 16:47:28 +0100

    fix: passthrough

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -13,16 +13,13 @@ attest_key=$(circleci env subst "${MANIFEST_ATTEST_PRIVATE_KEY}")
 hierarchical=$(circleci env subst "${MANIFEST_HIERARCHICAL_MERGE}")
 
 sources=${sources//,/}
-tptargs=$(echo "${ptargs}" | awk '{$1=$1};1')
-echo "transform args" "${tptargs}"
-echo "original args" "${ptargs}"
 
 # This is required for publishing
 MANFEST_API_KEY=${MANFEST_API_KEY:$(circleci env subst "${MANIFEST_PUBLISH_API_KEY}")}
 
-if [[ -z "${tptargs}" ]]; then
+if [[ -z "${ptargs}" ]]; then
     manifest sbom --source="CircleCI" --attest="${attest}" --key="${attest_key}" --hierarchical="${hierarchical}" --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output "${format}" --publish="${publish}" "${sources}"
 else
-    echo "using flags" "${tptargs}"
-    manifest sbom --source="CircleCI" --attest="${attest}" --key="${attest_key}" --hierarchical="${hierarchical}" --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output "${format}" --publish="${publish}" "${sources}" -- --spec-version 1.4
+    echo "using flags" "${ptargs}"
+    manifest sbom --source="CircleCI" --attest="${attest}" --key="${attest_key}" --hierarchical="${hierarchical}" --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output "${format}" --publish="${publish}" "${sources}" -- "${ptargs}"
 fi
