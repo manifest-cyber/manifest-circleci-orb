2aae975a269c0878f4d3e46191301d7bf3cb95dc
manifestori <ori@manifestcyber.com>
2023-11-01 13:38:05 +0100

    fix: build

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+trim() {
+    local value="$1"
+    # If the value is empty or just whitespace, return an empty string
+    [[ -z "${value// /}" ]] && echo "" || echo "$value"
+}
+
 labels=$(circleci env subst "${MANIFEST_LABELS}")
 generator=$(circleci env subst "${MANIFEST_GENERATOR}")
 name=$(circleci env subst "${MANIFEST_SOURCE_NAME}")
@@ -14,9 +20,10 @@ attest_key=$(circleci env subst "${MANIFEST_ATTEST_PRIVATE_KEY}")
 hierarchical=$(circleci env subst "${MANIFEST_HIERARCHICAL_MERGE}")
 
 sources=${sources//,/}
+api_key=$(trim "${api_key}")
 
 if [[ -z "${tptargs}" ]]; then
-    manifest sbom --source="CircleCI" --attest="${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label "${labels}" --generator "${generator}" --name "${name}" --version "${version}" --output "${format}" --publish="${publish}" "${sources}"
+    manifest sbom --source="CircleCI" --api-key="${api_key}" --attest="${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label "${labels}" --generator "${generator}" --name "${name}" --version "${version}" --output "${format}" --publish="${publish}" "${sources}"
 else
-    manifest sbom --source="CircleCI" --attest="${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label "${labels}" --generator "${generator}" --name "${name}" --version "${version}" --output "${format}" --publish="${publish}" "${sources}" -- "${ptargs}"
+    manifest sbom --source="CircleCI" --api-key="${api_key}" --attest="${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label "${labels}" --generator "${generator}" --name "${name}" --version "${version}" --output "${format}" --publish="${publish}" "${sources}" -- "${ptargs}"
 fi
