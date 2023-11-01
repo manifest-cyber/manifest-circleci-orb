234fef09f77cc2a89cdbddd6642f6684046cf4d8
manifestori <ori@manifestcyber.com>
2023-11-01 13:21:43 +0100

    fix: build

--- src/scripts/generate.sh
+++ src/scripts/generate.sh
@@ -14,10 +14,9 @@ attest_key=$(circleci env subst "${MANIFEST_ATTEST_PRIVATE_KEY}")
 hierarchical=$(circleci env subst "${MANIFEST_HIERARCHICAL_MERGE}")
 
 sources=${sources//,/}
-tptargs=$(echo "${ptargs}" | awk '{$1=$1};1')
 
 if [[ -z "${tptargs}" ]]; then
-    manifest sbom --source="CircleCI" --api-key "${api_key}" --attest "${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" --publish="${publish}" "${sources}"
+    manifest sbom --source="CircleCI" --api-key "${api_key}" --attest="${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label "${labels}" --generator "${generator}" --name "${name}" --version "${version}" --output "${format}" --publish="${publish}" "${sources}"
 else
-    manifest sbom --source="CircleCI" --api-key "${api_key}" --attest "${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label="${labels}" --generator="${generator}" --name="${name}" --version="${version}" --output="${format}" --publish="${publish}" "${sources}" -- "${ptargs}"
+    manifest sbom --source="CircleCI" --api-key "${api_key}" --attest="${attest}" --key "${attest_key}" --hierarchical="${hierarchical}" --label "${labels}" --generator "${generator}" --name "${name}" --version "${version}" --output "${format}" --publish="${publish}" "${sources}" -- "${ptargs}"
 fi
